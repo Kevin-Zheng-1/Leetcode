0577534dcc435201a3f961d89d45adb4d000106c
Kevin-Zheng-1 <75181681+Kevin-Zheng-1@users.noreply.github.com>
2020-12-12 14:27:08 -0800

    Math

---
+++ Algorithm/LinkedList/0002. Add Two Numbers.py
@@ -0,0 +1,27 @@
+# You are given two non-empty linked lists representing two non-negative integers. 
+# The digits are stored in reverse order, and each of their nodes contains a single digit. 
+# Add the two numbers and return the sum as a linked list.
+
+# You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        
+        carry = 0
+        root = n = ListNode(0)
+        
+        while l1 or l2 or carry:
+            v1 = v2 = 0
+            
+            if l1:
+                v1 = l1.val
+                l1 = l1.next
+            if l2:
+                v2 = l2.val
+                l2 = l2.next
+                
+            carry, val = divmod(v1+v2+carry, 10)
+            n.next = ListNode(val)
+            n = n.next
+            
+        return root.next
