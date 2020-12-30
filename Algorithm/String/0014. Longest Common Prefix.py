475f433d5378e5a46ddcc13ec7a3fea6f37e3703
Kevin-Zheng-1 <75181681+Kevin-Zheng-1@users.noreply.github.com>
2020-12-30 10:47:15 -0800

    Update 0014. Longest Common Prefix.py

--- Algorithm/String/0014. Longest Common Prefix.py
+++ Algorithm/String/0014. Longest Common Prefix.py
@@ -2,6 +2,7 @@
 
 # If there is no common prefix, return an empty string "".
 
+# Method 1
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         
@@ -19,3 +20,26 @@ class Solution:
         return s1
             
 # min()和max()两个函数应用于含有字符串的列表中时返回的值是基于字母的排序
+
+# Method 2
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        
+        result = ""
+        i = 0
+        
+        while True:
+            try:
+                sets = set([string[i] for string in strs])
+                if len(sets) == 1:
+                    result += sets.pop()
+                    i += 1
+                else:
+                    break
+            except:
+                break
+                
+        return result
+
+# 利用set()函数来寻找各字符串在相同位置上的唯一值的个数
+    
