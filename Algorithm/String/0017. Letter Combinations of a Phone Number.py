d0448b90e3d646d56e61b3a04d633655b123e90f
Kevin-Zheng-1 <75181681+Kevin-Zheng-1@users.noreply.github.com>
2021-01-03 23:32:56 -0800

    Recursion

---
+++ Algorithm/String/0017. Letter Combinations of a Phone Number.py
@@ -0,0 +1,39 @@
+# Given a string containing digits from 2-9 inclusive, 
+# return all possible letter combinations that the number could represent. 
+# Return the answer in any order.
+
+class Solution:
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        phone = {'2': ['a', 'b', 'c'],
+                 '3': ['d', 'e', 'f'],
+                 '4': ['g', 'h', 'i'],
+                 '5': ['j', 'k', 'l'],
+                 '6': ['m', 'n', 'o'],
+                 '7': ['p', 'q', 'r', 's'],
+                 '8': ['t', 'u', 'v'],
+                 '9': ['w', 'x', 'y', 'z']}
+                
+        def backtrack(combination, next_digits):
+            # if there is no more digits to check
+            if len(next_digits) == 0:
+                # the combination is done
+                output.append(combination)
+            # if there are still digits to check
+            else:
+                # iterate over all letters which map 
+                # the next available digit
+                for letter in phone[next_digits[0]]:
+                    # append the current letter to the combination
+                    # and proceed to the next digits
+                    backtrack(combination + letter, next_digits[1:])
+                    
+        output = []
+        if digits:
+            backtrack("", digits)
+        return output
+        
+# 运用递归思想，想象一个树形结构，从底部挨个寻找，当一个结点的所有分支都已遍历，则指针移到同级的结点或父结点      
