40ac710260f895d7714a10c4767cdf0737eaba32
Kevin-Zheng-1 <75181681+Kevin-Zheng-1@users.noreply.github.com>
2021-01-07 11:35:02 -0800

    Recursion

---
+++ Algorithm/Array/0018. 4Sum.py
@@ -0,0 +1,37 @@
+# Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums such that a + b + c + d = target? 
+# Find all unique quadruplets in the array which gives the sum of target.
+# Notice that the solution set must not contain duplicate quadruplets.
+
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        def kSum(nums: List[int], target: int, k: int) -> List[List[int]]:
+            res = []
+            if len(nums) == 0 or nums[0] * k > target or target > nums[-1] * k:
+                return res
+            if k == 2:
+                return twoSum(nums, target)
+            for i in range(len(nums)):
+                if i == 0 or nums[i - 1] != nums[i]:
+                    for _, set in enumerate(kSum(nums[i + 1:], target - nums[i], k - 1)):
+                        res.append([nums[i]] + set)
+            return res
+
+        def twoSum(nums: List[int], target: int) -> List[List[int]]:
+            res = []
+            lo, hi = 0, len(nums) - 1
+            while (lo < hi):
+                sum = nums[lo] + nums[hi]
+                if sum < target or (lo > 0 and nums[lo] == nums[lo - 1]):
+                    lo += 1
+                elif sum > target or (hi < len(nums) - 1 and nums[hi] == nums[hi + 1]):
+                    hi -= 1
+                else:
+                    res.append([nums[lo], nums[hi]])
+                    lo += 1
+                    hi -= 1
+            return res
+
+        nums.sort()
+        return kSum(nums, target, 4)
+        
+# 这题是让求4Sum，本质上可以延伸为求kSum，用一个递归，当k>2时，循环k-2个数，k=2时调用2Sum函数        
